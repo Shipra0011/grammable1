8f55ca688aa6d76575b160741619b43e60d529f2
Shipra0011 <surfriderfitness23@gmail.com>
2017-02-17 17:27:16 -0500

    test

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,11 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!, only: [:create]
 
-	def create
+  def create
     @gram = Gram.find_by_id(params[:gram_id])
     return render_not_found if @gram.blank?
+    @gram.comments.create(comment_params.merge(user: current_user))
+    redirect_to root_path
   end
 
   private
